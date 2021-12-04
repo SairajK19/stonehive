c6833ff02fe218e2d15bbd177b7f7cff0437e508
SairajK19 <saikapdi11@gmail.com>
2021-12-04 13:51:22 +0530

    updated sidebar items

--- src/components/Sidebar/sidebarItems.js
+++ src/components/Sidebar/sidebarItems.js
@@ -22,24 +22,36 @@ export const clientSidebarItems = [
 ];
 export const superuserSidebarItems = [
   {
-    name: "projects",
-    link: "/projects",
+    name: "Dashboard",
+    link: "/dashboard",
     icon: "bx:bxs-dashboard",
   },
   {
-    name: "activities",
-    link: "/activities",
+    name: "Complaints",
+    link: "/complaints",
     icon: "fluent:timeline-20-filled",
   },
   {
-    name: "budget",
-    link: "/budget",
-    icon: "ic:baseline-analytics",
+    name: "Bills",
+    link: "/bills",
+    icon: "fa-solid:money-check-alt",
   },
   {
-    name: "budget",
-    link: "/budget1",
-    icon: "ic:baseline-analytics",
+    name: "Architect",
+    link: "/architect",
+    icon: "clarity:design-line",
+  },
+];
+export const superuserHomeSidebarItems = [
+  {
+    name: "Projects",
+    link: "/projects",
+    icon: "whh:projectsend",
+  },
+  {
+    name: "Inquiries",
+    link: "/inquiries",
+    icon: "fluent:mail-24-filled",
   },
 ];
 export const contractorSidebarItems = [
