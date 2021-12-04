ad86aa2a34190592fd2c7c727a645af3d6bae071
SairajK19 <saikapdi11@gmail.com>
2021-12-04 18:47:20 +0530

    started projects page

--- src/pages/Dashboard/Superuser/Projects.js
+++ src/pages/Dashboard/Superuser/Projects.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { superuserHomeSidebarItems } from "../../../components/Sidebar/sidebarItems";
+import { setSidebarItems } from "../../../redux/reducers/shReducers";
+import styles from "./styles/projects.module.scss";
+import { Icon } from "@iconify/react";
 
 export default function Projects() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSidebarItems(superuserHomeSidebarItems));
+  });
+
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.container}>
+      {/* Project list */}
+      <div className={styles.list}>
+        {/* Project list search panel */}
+        <div className={styles.search}>
+          <h1>Projects</h1>
+          <div className={styles.search_bar}>
+            <Icon icon="fe:search" />
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+
+        {/* Project list filter panel */}
+        <div>
+          {/* Navigation */}
+          <div>
+            <div>
+              <p>Ongoing</p>
+              <span></span>
+            </div>
+            <div>
+              <p>Past</p>
+              <span></span>
+            </div>
+            <div>
+              <p>Archived</p>
+              <span></span>
+            </div>
+            <div>
+              <p>All</p>
+              <span></span>
+            </div>
+          </div>
+
+          {/* Create project button */}
+          <div>
+            <Icon icon="gridicons:create" />
+            <p>Create Project</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Important Details, Deadlines */}
+      <div></div>
     </div>
   );
 }
