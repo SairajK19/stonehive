7ab2829c5a7975373d378b75d9550bea6450b9e1
SairajK19 <saikapdi11@gmail.com>
2021-12-24 21:33:26 +0530

    added architect page for SU

--- src/pages/Dashboard/Superuser/dashboard/Architect/SuperUserArchitect.js
+++ src/pages/Dashboard/Superuser/dashboard/Architect/SuperUserArchitect.js
@@ -1,5 +1,95 @@
-import React from "react";
+import { Icon } from "@iconify/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import SearchBar from "../../../../../components/SearchBar/SearchBar";
+import { superuserSidebarItems } from "../../../../../components/Sidebar/sidebarItems";
+import { setSidebarItems } from "../../../../../redux/reducers/shReducers";
+import styles from "./styles/SuperUserArchitect.module.scss";
+import plan from "../../../../../assets/images/plan.png";
 
 export default function SuperUserArchitect() {
-  return <div>ARCHITECT</div>;
+  // State variables
+  const [navigation, setNavigation] = useState([
+    { item_name: "All", style: "all", checked: true },
+    { item_name: "Archived", style: "archived", checked: false },
+  ]);
+  const [overlay, setOverlay] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      setSidebarItems({ active: "Architect", items: superuserSidebarItems })
+    );
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {overlay ? (
+        <span
+          className={styles.overlay}
+          // onClick={() => handleThreeDotMenu(currentMenu3Dot)}
+        ></span>
+      ) : (
+        ""
+      )}
+
+      <div className={styles.search}>
+        <h1>All Uploads</h1>
+
+        {/* Search Bar */}
+        <SearchBar />
+      </div>
+
+      <div className={styles.filter}>
+        {/* Navigation */}
+        <div className={styles.navigation}>
+          {navigation.map((item) => (
+            <div className={styles.filter_item}>
+              <label>
+                <input
+                  type="radio"
+                  name="filter-item"
+                  id={styles[item.style]}
+                  className={styles.filter_item_input}
+                  defaultChecked={item.checked}
+                />
+                <span className={styles.filter_item_text}>
+                  {item.item_name}
+                </span>
+                <span className={styles.controlme}></span>
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.plans}>
+        <div className={styles.list}>
+          {[1, 2, 3, 4, 5, 6].map((item) => (
+            <Plan />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const Plan = () => {
+  return (
+    <div className={styles.plan}>
+      <div className={styles.plan_img}>
+        <img src={plan} />
+      </div>
+      <div className={styles.plan_options}>
+        <h3>Ground floor plan</h3>
+        <div className={styles.more}>
+          <Icon
+            icon="akar-icons:more-horizontal"
+            style={{ fontSize: "25px" }}
+          />
+        </div>
+        <p>20 august 2021</p>
+      </div>
+    </div>
+  );
+};
