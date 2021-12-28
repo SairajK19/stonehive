84f2e616a4322b1d447b62cf889607be70f19aeb
SairajK19 <saikapdi11@gmail.com>
2021-12-28 12:58:07 +0530

    added overlay for popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./Popup.module.scss";
 
-export default function Popup({ Component, handlePopupToggle }) {
+export default function Popup({ Component, handlePopupToggle, popupName }) {
   return (
     <span className={styles.popup} id="popup">
+      <span
+        className={styles.overlay}
+        onClick={() => handlePopupToggle(popupName)}
+      ></span>
+
       <Component handlePopupToggle={handlePopupToggle} />
     </span>
   );
