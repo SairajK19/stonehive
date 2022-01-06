e834d995f91087541d709d7c3761d1bea598c98b
SairajK19 <saikapdi11@gmail.com>
2022-01-06 11:29:32 +0530

    Now bills can be viewed

--- src/pages/Dashboard/Superuser/dashboard/Bills/SuperUserBills.js
+++ src/pages/Dashboard/Superuser/dashboard/Bills/SuperUserBills.js
@@ -3,19 +3,62 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { superuserSidebarItems } from "../../../../../components/Sidebar/sidebarItems";
 import { setSidebarItems,setTopBarVisibility } from "../../../../../redux/reducers/shReducers";
+import { createLocalUrl } from "../../../../../helpers/helpers";
 import styles from "./styles/SuperUserBills.module.scss";
+import testBill from "../../../../../assets/testBill.pdf";
 
 export default function SuperUserBills() {
   const dispatch = useDispatch();
   const [showFilter, setShowFilter] = useState(false);
   const [showOverlay, setShowOverlay] = useState(false);
   const [filter, setFilter] = useState("All Bills");
+  const [bills, setBills] = useState([
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30", approved: true },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
+          ]);
   useEffect(() => {
     dispatch(
       setSidebarItems({ active: "Bills", items: superuserSidebarItems })
     );
     dispatch(setTopBarVisibility({ visibility: true }));
   }, []);
+
+  useEffect(() => {
+    /* 
+     * Code from line 39 to 54 is temperory,
+     * since we don't get the file as a blob
+     * after importing it I have written this code
+     * to get the blob of the pdf using the path.
+     */
+    const oReq = new XMLHttpRequest();
+
+    oReq.open("GET", testBill, true);
+
+    oReq.responseType = "blob";
+
+    oReq.onload = () => {
+        const file = new Blob([oReq.response], { type: 'application/pdf' });
+        const url = createLocalUrl(file);
+
+        // Add the url to bills array.
+        bills.map((bill) => {
+            bill["bill_url"] = url;
+        })
+    }
+    oReq.send();
+
+    /*
+     * In reality it will just be 
+     * const url = createLocalUrl(file);
+     * as file will be stored as blob in the database
+     */
+  },[])
 
   const handleOverlayClick = () => {
     setShowFilter(!showFilter);
@@ -66,31 +109,14 @@ export default function SuperUserBills() {
 
       <div className={styles.bills}>
         <div className={styles.list}>
-          {[
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30", approved: true },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-            { bill_label: "Bill 99", date_time: "Sep 05 9:30" },
-          ].map((bill) => (
+          {bills.map((bill) => (
             <div className={styles.bill}>
               <div className={styles.bill_info}>
                 <h2>Bill 99</h2>
                 <p>Sep 5 9:30</p>
               </div>
               <div className={styles.bill_button}>
-                  <a id={styles.view}>
-                      {/* Here once we receive the bill object file, we need to create a local url using
-                        URL.createObjectURL(), it takes an argument that is the pdf object we get from the database.
-                        so the bill can be seen locally.
-
-                        Check
-                        https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-                        for more info
-                        */}
+                  <a id={styles.view} href={bill.bill_url} target="_blank" >
                       <Icon icon="akar-icons:eye" />
                       View
                   </a>
