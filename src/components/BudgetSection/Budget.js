8a1a383cc745bb8f497655b3171a8834b282f83f
c0deb1ind31 <rhutikparab1@gmail.com>
2022-01-08 21:11:14 +0530

    Added Budget Section

---
+++ src/components/BudgetSection/Budget.js
@@ -0,0 +1,83 @@
+import React, { useContext, useEffect } from "react";
+import BudgetChart from "../BudgetChart/BudgetChart";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { Icon } from "@iconify/react";
+
+import PhaseSelector, {
+  SelectedPhaseContext,
+} from "../PhaseSelector/PhaseSelector";
+
+import styles from "./budget.module.scss";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = [""];
+export const options = {
+  indexAxis: "y",
+  respondive: true,
+  plugins: {
+    legend: {
+      position: "left",
+    },
+  },
+};
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Actual",
+      data: [10],
+      backgroundColor: "rgba(251, 147, 109, 1)",
+    },
+    {
+      label: "Estimated",
+      data: [50],
+      backgroundColor: "rgba(128, 201, 179, 1)",
+    },
+  ],
+};
+
+export default function Budget() {
+  const SelectedPhase = useContext(SelectedPhaseContext);
+  useEffect(() => {
+    console.log(SelectedPhase);
+  }, [SelectedPhase]);
+  return (
+    <div className={styles.container}>
+      <div className={styles.budget_charts}>
+        {SelectedPhase}
+        <BudgetChart />
+      </div>
+      <div className={styles.phases}>
+        <PhaseSelector />
+      </div>
+      <div className={styles.budget_graph}>
+        <p id={styles.title}>Actual vs Estimated Budget</p>
+        <p id={styles.budget_difference}>
+          <Icon icon="bx:bxs-up-arrow" color="#82D616" height="20" />
+          4% more than planned budget
+        </p>
+        <Bar
+          options={options}
+          data={data}
+          style={{ maxHeight: "60%", maxWidth: "100%" }}
+        />
+      </div>
+    </div>
+  );
+}
