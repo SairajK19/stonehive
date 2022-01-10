314193afe044003730bab74f527ac3fe70b081f1
c0deb1ind31 <rhutikparab1@gmail.com>
2022-01-10 14:39:09 +0530

    Added Task Page

---
+++ src/pages/Dashboard/Contractor/Tasks/Tasks.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { contractorSidebarItems } from "../../../../components/Sidebar/sidebarItems";
+import { setSidebarItems } from "../../../../redux/reducers/shReducers";
+import ActivityTaskTable from "../../../../components/ActivityTaskTable/ActivityTaskTable";
+import FinancialCards from "../../../../components/FinanclalCards/FinancialCards";
+import PhaseSelector from "../../../../components/PhaseSelector/PhaseSelector";
+
+import styles from "./task.module.scss";
+import "../../../../styles/dashboard.scss";
+export default function Tasks({ fromPopup = false }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!fromPopup) {
+      dispatch(
+        setSidebarItems({ active: "tasks", items: contractorSidebarItems })
+      );
+    }
+  });
+  return (
+    <div
+      className={
+        !fromPopup
+          ? `${styles.container} responsive_font p-20`
+          : `${styles.container} responsive_font`
+      }
+    >
+      <div className="task_table">
+        <ActivityTaskTable contractorDash={true} />
+      </div>
+      <div className="phases">
+        <PhaseSelector />
+      </div>
+      <div className="">
+        <FinancialCards />
+      </div>
+    </div>
+  );
+}
