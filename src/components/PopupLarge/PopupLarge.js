99b82d6879505c5c6d2e3fdad90ac9ddbb17608d
c0deb1ind31 <rhutikparab1@gmail.com>
2022-01-10 14:49:36 +0530

    Moved Popup to Component

---
+++ src/components/PopupLarge/PopupLarge.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import styles from "./popuplarge.module.scss";
+import { Icon } from "@iconify/react";
+export default function PopupLarge({
+  children,
+  popupToggle,
+  setPopupToggle,
+  popupTitle,
+}) {
+  useEffect(() => {
+    window.addEventListener("keypress", (e) => {
+      if (e.code === 27) {
+        setPopupToggle(false);
+      }
+    });
+  });
+  return (
+    <div
+      className={styles.container}
+      style={popupToggle ? { visibility: "visible" } : { visibility: "hidden" }}
+    >
+      <div
+        className={styles.overlay}
+        onClick={() => setPopupToggle(false)}
+      ></div>
+      <div className={styles.main_container}>
+        <div className={styles.popup_head}>
+          {" "}
+          <p className={styles.popup_title}>{popupTitle}</p>
+          <button
+            className={styles.close_btn}
+            onClick={() => setPopupToggle(false)}
+          >
+            <Icon icon="clarity:window-close-line" height="30" />
+          </button>
+        </div>
+
+        {children}
+      </div>
+    </div>
+  );
+}
