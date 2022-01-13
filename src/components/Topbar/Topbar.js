f58d16e949698f2993dd3255ae9087bd663ebd16
SairajK19 <saikapdi11@gmail.com>
2022-01-13 19:36:16 +0530

    Added a little animation to dropdown

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -5,74 +5,74 @@ import ProfileImg from "../../assets/images/profile.png";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 export default function Topbar() {
-  const active_item = useSelector(
-    (state) => state.stonehive.sidebarItems.active
-  );
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [showOverlay, setShowOverlay] = useState(false);
+    const active_item = useSelector(
+        (state) => state.stonehive.sidebarItems.active
+    );
+    const [showDropdown, setShowDropdown] = useState(true);
+    const [showOverlay, setShowOverlay] = useState(false);
 
-    const handleOverlayClick = () => {
+    const handleOverlayClick = (component) => {
         const icon = document.getElementById("icon");
+        const checkbox = document.getElementsByName("dropdown_checkbox")[0];
 
         (icon.style.transform === "rotate(0deg)") ? 
-            icon.style.transform = "rotate(180deg)" : icon.style.transform = "rotate(0deg)" 
-        
+            icon.style.transform = "rotate(180deg)" : icon.style.transform = "rotate(0deg)";
+
+        if (component === "overlay") { checkbox.checked = false; console.log(checkbox.checked) }
         setShowDropdown(!showDropdown);
         setShowOverlay(!showOverlay);
     }
-  return (
-    <div className={`${styles.container} responsive_font`}>
-      {showOverlay ? (
-        <span
-          className={styles.container_overlay}
-          onClick={handleOverlayClick}
-        ></span>
-      ) : (
-        ""
-      )}
-      <div className={styles.headings}>
-        <div className={styles.headings_col}>
-          <h2>{active_item}</h2>
-          <h5>
-            Welcome back sairaj
-            <Icon
-              icon="emojione:waving-hand-medium-light-skin-tone"
-              height="20"
-            />
-          </h5>
+    return (
+        <div className={`${styles.container} responsive_font`}>
+            {showOverlay ? (
+                <span
+                    className={styles.container_overlay}
+                    onClick={() => handleOverlayClick("overlay")}
+                ></span>
+            ) : (
+                ""
+            )}
+            <div className={styles.headings}>
+                <div className={styles.headings_col}>
+                    <h2>{active_item}</h2>
+                    <h5>
+                        Welcome back sairaj
+                        <Icon
+                            icon="emojione:waving-hand-medium-light-skin-tone"
+                            height="20"
+                        />
+                    </h5>
+                </div>
+            </div>
+            <div className={styles.user_profile_notifi}>
+                <Icon icon="ic:baseline-circle-notifications" width="30px" />
+                <div className={styles.seperator}></div>
+                <div className={styles.profile}>
+                    <img src={ProfileImg} alt="prifle" />
+                </div>
+                <label className={styles.dropdown_container}>
+                    <input type="checkbox" id={styles.dropdown_checkbox} name="dropdown_checkbox" onClick={handleOverlayClick} />
+                    <div className={styles.dropdown_icon} onClick={handleOverlayClick} >
+                        <Icon icon="ls:dropdown" width="20px" id="icon" className={styles.icon} />
+                    </div>
+                    <div className={styles.dropdown} id={styles.dropdown} >
+                        <div className={styles.name}>
+                            <p>Signed in as</p>
+                            <p>Sairaj Kapdi</p>
+                        </div>
+                        <div className={styles.navs}>
+                            <div className={styles.item}>
+                                <Icon icon="ci:settings-filled" />
+                                <Link to="/client/settings" >Settings</Link>
+                            </div>
+                            <div className={styles.item}>
+                                <Icon icon="ri:logout-circle-r-fill" />
+                                <Link to="/client/settings" >Logout</Link>
+                            </div>
+                        </div>
+                    </div>
+                </label>
+            </div>
         </div>
-      </div>
-      <div className={styles.user_profile_notifi}>
-        <Icon icon="ic:baseline-circle-notifications" width="30px" />
-        <div className={styles.seperator}></div>
-        <div className={styles.profile}>
-          <img src={ProfileImg} alt="prifle" />
-        </div>
-          <div className={styles.dropdown_container}>
-              <div className={styles.dropdown_icon} onClick={handleOverlayClick} >
-                  <Icon icon="ls:dropdown" width="20px" id="icon" className={styles.icon} />
-              </div>
-              {
-                  showDropdown ? 
-                      <div className={styles.dropdown}>
-                          <div className={styles.name}>
-                              <p>Signed in as</p>
-                              <p>Sairaj Kapdi</p>
-                          </div>
-                          <div className={styles.navs}>
-                              <div className={styles.item}>
-                                  <Icon icon="ci:settings-filled" />
-                                  <Link to="/client/settings" >Settings</Link>
-                              </div>
-                              <div className={styles.item}>
-                                  <Icon icon="ri:logout-circle-r-fill" />
-                                  <Link to="/client/settings" >Logout</Link>
-                              </div>
-                          </div>
-                      </div> : ""
-              }
-          </div>
-      </div>
-    </div>
-  );
+    );
 }
