f8e3d2bc7c8c924226421a4a36457a8ba69cf877
SairajK19 <saikapdi11@gmail.com>
2022-01-13 16:43:39 +0530

    added dropdown

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./topbar.module.scss";
 import { Icon } from "@iconify/react";
 import ProfileImg from "../../assets/images/profile.png";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export default function Topbar() {
   const active_item = useSelector(
     (state) => state.stonehive.sidebarItems.active
   );
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    const checkbox = document.getElementById("dropdown_icon");
+
+    checkbox.addEventListener("change", () => {
+        const icon = document.getElementById("icon");
+
+        (icon.style.transform === "rotate(0deg)") ? 
+            icon.style.transform = "rotate(180deg)" : icon.style.transform = "rotate(0deg)" 
+    })
+  }, [])
 
   return (
     <div className={`${styles.container} responsive_font`}>
@@ -28,7 +41,31 @@ export default function Topbar() {
         <div className={styles.profile}>
           <img src={ProfileImg} alt="prifle" />
         </div>
-        <Icon icon="ls:dropdown" width="20px"/>
+          <div className={styles.dropdown_container}>
+              <div className={styles.dropdown_icon} >
+                  <input type="checkbox" id="dropdown_icon" onClick={() => setShowDropdown(!showDropdown)} />
+                  <Icon icon="ls:dropdown" width="20px" id="icon" className={styles.icon} />
+              </div>
+              {
+                  showDropdown ? 
+                      <div className={styles.dropdown}>
+                          <div className={styles.name}>
+                              <p>Signed in as</p>
+                              <p>Sairaj Kapdi</p>
+                          </div>
+                          <div className={styles.navs}>
+                              <div className={styles.item}>
+                                  <Icon icon="ci:settings-filled" />
+                                  <Link to="/client/settings" >Settings</Link>
+                              </div>
+                              <div className={styles.item}>
+                                  <Icon icon="ri:logout-circle-r-fill" />
+                                  <Link to="/client/settings" >Logout</Link>
+                              </div>
+                          </div>
+                      </div> : ""
+              }
+          </div>
       </div>
     </div>
   );
